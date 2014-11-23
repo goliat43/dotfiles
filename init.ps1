751b881aa8046468caf40b578ed22b46c8165aef
Niclas Eriksson <niclas@eriksson.cc>
2014-11-23 14:16:30 +0100

    * Add init script. * Update some settings.

--- init.ps1
+++ init.ps1
@@ -1,4 +1,32 @@
-cmd /c mklink /H $ENV:USERPROFILE\.gitconfig .gitconfig
-cmd /c mklink /H $ENV:USERPROFILE\_vimrc _vimrc
-cmd /c mklink /H $ENV:USERPROFILE\_vimperatorrc _vimperatorrc
-cmd /c mklink /H $ENV:USERPROFILE\AutoHotkey.ahk AutoHotkey.ahk
+$gitconfig = "$ENV:USERPROFILE\.gitconfig" 
+$vimrc = "$ENV:USERPROFILE\_vimrc"
+$vimperatorrc = "$ENV:USERPROFILE\_vimperatorrc"
+$ahkConfig = "$ENV:USERPROFILE\AutoHotkey.ahk"
+
+if(Test-Path $gitconfig) {
+    rm -Confirm $gitconfig
+}
+if(Test-Path $vimrc) {
+    rm -Confirm $vimrc
+}
+if(Test-Path $vimperatorrc) {
+    rm -Confirm $vimperatorrc
+}
+if(Test-Path $ahkConfig) {
+    rm -Confirm $ahkConfig
+}
+
+cmd "/c mklink /H $gitconfig .gitconfig"
+cmd "/c mklink /H $vimrc _vimrc"
+cmd "/c mklink /H $vimperatorrc _vimperatorrc"
+cmd "/c mklink /H $ahkConfig AutoHotkey.ahk"
+
+#Create git user config
+$gitUser = "$ENV:USERPROFILE\.gitconfig.user"
+if(!(Test-Path $gitUser)) {
+    Add-Content -Path $gitUser -Value "[User]"
+    $user = Read-Host -Promp "Username for git?"
+    $userEmail = Read-Host -Prompt "Email for git?"
+    Add-Content -Path $gitUser -Value "`tname = $user"
+    Add-Content -Path $gitUser -Value "`temail = $userEmail"
+}
