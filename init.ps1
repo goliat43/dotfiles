727236eb937dfc8e508f1739dd01fa25c9c3fef6
niclase <niclas.eriksson@knowit.se>
2018-02-14 15:13:57 +0100

    Slightly updated configs.

--- init.ps1
+++ init.ps1
@@ -1,36 +1,52 @@
 $gitconfig = "$ENV:USERPROFILE\.gitconfig" 
 $vimrc = "$ENV:USERPROFILE\.vimrc.local"
+$bashrc = "$ENV:USERPROFILE\.bashrc"
 $vimperatorrc = "$ENV:USERPROFILE\_vimperatorrc"
 $ahkConfig = "$ENV:USERPROFILE\AutoHotkey.ahk"
 $vsvimrc = "$ENV:USERPROFILE\.vsvimrc"
 $psprofile = $profile.CurrentUserAllHosts
+$conemuconfig = """C:\Program Files\ConEmu\ConEmu.xml"""
 
-if(Test-Path $gitconfig) {
-    rm -Confirm $gitconfig
+if(Test-Path $gitconfig) {    
+	rm -Confirm $gitconfig
 }
+cmd "/c mklink /H $gitconfig .gitconfig"
+
+
 if(Test-Path $vimrc) {
     rm -Confirm $vimrc
 }
+cmd "/c mklink /H $vimrc .vimrc.local"
+
 if(Test-Path $vimperatorrc) {
     rm -Confirm $vimperatorrc
 }
+cmd "/c mklink /H $vimperatorrc _vimperatorrc"
+
 if(Test-Path $ahkConfig) {
-    rm -Confirm $ahkConfig
+    rm -Confirm $ahkConfig	
 }
+cmd "/c mklink /H $ahkConfig AutoHotkey.ahk"
+
 if(Test-Path $vsvimrc) {
     rm -Confirm $vsvimrc
 }
+cmd "/c mklink /H $vsvimrc .vsvimrc"
+
 if(Test-Path $psprofile) {
     rm -Confirm $psprofile
 }
-
-
-cmd "/c mklink /H $gitconfig .gitconfig"
-cmd "/c mklink /H $vimrc .vimrc.local"
-cmd "/c mklink /H $vimperatorrc _vimperatorrc"
-cmd "/c mklink /H $ahkConfig AutoHotkey.ahk"
-cmd "/c mklink /H $vsvimrc .vsvimrc"
 cmd "/c mklink /H $psprofile profile.ps1"
+
+if(Test-Path $bashrc) {
+    rm -Confirm $bashrc	
+}
+cmd "/c mklink /H $bashrc .bashrc"
+
+if(Test-Path $conemuconfig) {
+    rm -Confirm $conemuconfig	
+}
+cmd "/c mklink /H $conemuconfig conemu.xml"
 
 #git clone https://github.com/powerline/fonts.git
 
--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,7 @@
 #Current user all hosts
 
+Remove-Item alias:wget
+
 function which {	
 	$a = $args
 	$res = Get-Command $a
@@ -12,5 +14,21 @@ function which {
 	
 }
 
-set-alias np "C:\Program Files (x86)\Notepad++\notepad++.exe"
+#function touch {
+#
+#	echo $null >> $args
+#}
+ 	
+function whoami { (get-content env:\userdomain) + "\" + (get-content env:\username) }
+
+set-alias np "C:\Program Files\Notepad++\notepad++.exe"
+set-alias sudo elevate-process
+set-alias g git
+set-alias ssh 'C:\Program Files\Git\usr\bin\ssh.exe'
+
+Import-Module pscx
 Import-Module z
+Import-VisualStudioVars 2017
+
+. "$PSScriptRoot\PSReadline.ps1"
+#. "$PSScriptRoot\setfiletime.ps1"
